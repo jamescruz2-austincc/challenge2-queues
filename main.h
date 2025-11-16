064a166a97a3d6d517ae07eb5ec32d1d92782c43
James <james.cruz2@g.austincc.edu>
2025-11-15 22:51:01 -0600

    added file comment headers to all files

---
+++ main.h
@@ -0,0 +1,7 @@
+/* *************************************************
+*  Name: James Cruz
+*  Assignment: Challenge 2: Queues
+*  Purpose: Building an ADT Queue, building off of
+*           learnings from Stack and Linked List
+*  
+************************************************* */
